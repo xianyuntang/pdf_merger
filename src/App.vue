784f95dd01497acbddbbe9cf6bd5466795bc8894
shawn <shawn.xy.tang@mail.foxconn.com>
2022-01-05 13:53:45 +0800

    add pdftk support

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,10 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,5 +27,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
 </style>
--- src/backend/index.js
+++ src/backend/index.js
@@ -3,8 +3,6 @@ import {ipcMain, dialog} from 'electron'
 const {exec} = require('child_process');
 import path from "path";
 
-const PDFMerger = require('pdf-merger-js');
-
 export function registerListener() {
     ipcMain.on('start-merge', (event, args) => {
         console.log(args)
@@ -14,25 +12,25 @@ export function registerListener() {
 
             ]
         }).then(async (result) => {
-            await merge(result.filePath, args).then(() => {
-                event.reply('stop-merge', {'message': '成功'})
+            let command = `${path.join(process.cwd(), 'extraFiles', 'pdftk.exe')}`
+            args.forEach(item => {
+                command += ` "${item}"`
             })
+            command += ' cat'
+            command += ` output "${result.filePath}"`
+
+            console.log(command)
+            exec(command, function (err, stdout, stderr) {
+                if (err === null) {
+                    event.reply('stop-merge', {'message': '成功'})
+                } else {
+                    event.reply('stop-merge', {'message': stderr})
+                }
+            });
 
 
         }).catch(err => {
             console.log(err)
         })
     })
-}
-
-async function merge(filePath, fileList) {
-    let merger = new PDFMerger();
-
-    await (async () => {
-        for (let i = 0; i < fileList.length; i++) {
-            merger.add(fileList[i])
-        }
-
-        await merger.save(filePath)
-    })()
 }
